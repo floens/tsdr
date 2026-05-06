8380fcdf77bb3aea8e8676c790aa047303e6ddc5
Florens <floens@gmail.com>
2026-05-06 22:17:23 +0530

    Log startup warning on first run
    Cold start can take a long time due to numba jitting.

--- src/tsdr/__init__.py
+++ src/tsdr/__init__.py
@@ -1,4 +1,8 @@
-from tsdr.tui import TSDRApp
+import sys
+
+print("Starting TSDR (this may take a moment on first run)...", file=sys.stderr, flush=True)
+
+from tsdr.tui import TSDRApp  # noqa: E402
 
 __version__ = "0.1.0"
 __all__ = ["__version__", "TSDRApp"]
